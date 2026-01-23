7afb6d0f3adb51513f27d1f17ba839cc384d015a
ARPGC <ecobirlaapp@gmail.com>
2026-01-23 20:04:06 +0530

    Create supabase.js

---
+++ ojas/supabase.js
@@ -0,0 +1,7 @@
+// ojas/supabase.js
+
+// TODO: Replace with your NEW OJAS Project Credentials
+const SUPABASE_URL = 'https://sijmmlhltkksykhbuatn.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_9GjhwaWzz0McozvxVMINyQ_ZFU58z7F';
+
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
